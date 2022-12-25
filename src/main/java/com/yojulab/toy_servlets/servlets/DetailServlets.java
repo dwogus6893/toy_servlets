34236f6033fc4ca67154dc6e10c717de50385564
dwogus6893 <dwogus6893@naver.com>
2022-12-26 00:52:11 +0900

    dd

--- src/main/java/com/yojulab/toy_servlets/servlets/DetailServlets.java
+++ src/main/java/com/yojulab/toy_servlets/servlets/DetailServlets.java
@@ -1,6 +1,10 @@
 package com.yojulab.toy_servlets.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.yojulab.toy_servlets.dao.PollWithDB;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,6 +17,18 @@ public class DetailServlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse HttpServletResponse)
         throws ServletException, IOException {
-    // /polls/PollServlet?QUESTIONS_UID=Q1
+  
+        String example_Uid = request.getParameter("EXAMPLE_UID");
+        
+        //DB 객체 생성 및 LIST 받기
+        PollWithDB pollWithDB = new PollWithDB();
+        HashMap<String,Object> bundle_list = null;
+        ArrayList<String> answers = null;
+
+        try{
+            bundle_list = pollWithDB.getList(example.Uid);
+            HashMap<String,Object>
         }
+
+    }
 }
